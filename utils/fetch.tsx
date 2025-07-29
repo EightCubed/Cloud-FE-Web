03dfd86471aa0ca3e1447131a649bd56f80484a1
EightCubed <sushyam.s@gmail.com>
2025-07-29 16:44:36 +0530

    feat: Setting prod backend URL

--- utils/fetch.tsx
+++ utils/fetch.tsx
@@ -1,7 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-export const BACKEND_URL = "http://localhost:8080/";
-// export const BACKEND_URL = "http://192.168.29.16:8080/";
+export const BACKEND_URL = "https://test.eightcubed.site/api/";
 
 type MethodType = "put" | "post" | "get" | "patch" | "delete";
 
