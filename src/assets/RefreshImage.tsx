7fa1f4d38c4ed617b21ccdd23aa85a04978e191b
sushyam-yadav301-ibm <sushyam-yadav301@ibm.com>
2025-08-03 02:38:00 +0530

    feat: Updated refresh button size

--- src/assets/RefreshImage.tsx
+++ src/assets/RefreshImage.tsx
@@ -1,8 +1,8 @@
 const RefreshImage = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="40"
-    height="40"
+    width="20"
+    height="20"
     fill="none"
     viewBox="0 0 24 24"
   >
